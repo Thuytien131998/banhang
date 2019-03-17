1fd273013b1b91fb2c6395eabafd6ee38aee7393
Thuytien131998 <Tiennt62@wru.vn>
2019-03-17 20:12:05 +0700

    banhang

---
+++ banhang/index.php
@@ -0,0 +1,21 @@
+<?php
+include "config.php";//sao chep toan bo  file chi dinh vao file nay
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Juno</title>
+</head>
+<body>
+    <div class="container-fluid"><!--phan dau trang-->
+    </div>
+
+    <div class="content"><!--phan noi dung-->
+    </div>
+
+    <div class="footer"><!--cuoi trang->
+    </div>
+</body>
+</html>
