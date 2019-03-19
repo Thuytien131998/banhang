d29edeab3fb36cb694319ba393394fcfeee65838
Thuytien131998 <Tiennt62@wru.vn>
2019-03-19 22:29:42 +0700

    banhang

--- View/master.php
+++ View/master.php
@@ -4,9 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Juno</title>
-    <script src="<?php echo $url;?>js/bootstrap.min.js"></script>
-    <script src="<?php echo $url; ?>js/jquery-1.11.3.min.js"></script>
-    <link rel="stylesheet" href="">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
 </head>
 <body>
     <div class="container-fluid"><!--phan dau trang-->
@@ -15,14 +17,17 @@
          <div class="col-sm-3">
           <div class="top-logo-center">
             <a href="">
-              <img src="img/juno.png" >
+              <img src="img/juno.png"  style="padding:2px;height: 100px;">
             </a>
           </div>
          </div>
          <div class="col-sm-5">
-          <form class="post" action=" ">
-             <input type="text" placeholder="Search" name="search">
-             <button type="submit" name="btn_search"><i class="glyphicon glyphicon-search"></i></button>
+          <form class="searchform" action=" " method="post">
+             <input type="text" placeholder="Ban can tim gi?" name="search">
+             <button type="submit" name="btn_search" style=" height:20px;">
+               <i class="glyphicon glyphicon-search">
+               </i>
+              </button>
           </form>
          </div>
          <div class="col-sm-4"></div>
